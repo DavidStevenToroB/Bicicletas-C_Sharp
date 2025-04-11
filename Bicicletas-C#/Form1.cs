7eb3c76cfd161a90ebc0ca3272116af8710e6cf4
David Steven T <davidtoro013125@gmail,com>
2025-04-11 17:10:18 -0500

    Agregar archivos de proyecto.

---
+++ Bicicletas-C#/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System;
+
+namespace Bicicletas_C_
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conexion = new MySqlConnection("server=localhost; database=thebigwhee; uid=root; pwd=;");
+
+        public Form1()
+        {
+            InitializeComponent();
+            VerificarConexion();
+        }
+
+        private void VerificarConexion()
+        {
+            try
+            {
+                conexion.Open();
+                MessageBox.Show("Conexión Exitosa");
+                conexion.Close();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error de conexión a MySQL: " + ex.Message + "\nCódigo de error: " + ex.Number);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
